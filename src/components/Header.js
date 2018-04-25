014b4254e8c2f989da3ddab594b9171d72281b97
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-04-25 12:07:14 -0700

    Change package.json

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 
-const Header = (props) => {
-  return (
+class Header extends Component {
+
+  componentWillMount(){
+    props.fetchSetting(1)
+  }
+
+  render() {
+    return(
     <View style={{flex: 2, flexDirection: 'row'}}>
-      <View style={{flex: 1, backgroundColor: '#F8C471'}} />
-      <View style={{flex: 1, backgroundColor: '#EC7063'}} />
+      <View style={{flex: 1, backgroundColor: '#F8C471'}}>{props}</View>
+      <View style={{flex: 1, backgroundColor: '#EC7063'}}>
+        // <Image
+        //   style={{width: 66, height: 58}}
+        //   source={{uri: {props.image_path}}}
+        //   />
+      </View>
       <View style={{flex: 1, backgroundColor: '#3498DB'}} />
     </View>
-  )
+  )}
 }
 
 export default Header;
+
+// const Header = (props) => {
+//   return (
+//     <View style={{flex: 2, flexDirection: 'row'}}>
+//       <View style={{flex: 1, backgroundColor: '#F8C471'}}>{props.title}</View>
+//       <View style={{flex: 1, backgroundColor: '#EC7063'}}>
+//         <Image
+//           style={{width: 66, height: 58}}
+//           source={{uri: {props.image_path}}}
+//           />
+//       </View>
+//       <View style={{flex: 1, backgroundColor: '#3498DB'}} />
+//     </View>
+//   )
+// }
--- src/util/setting_api_util.js
+++ src/util/setting_api_util.js
@@ -1,6 +1,6 @@
 
 
-export default const fetchSetting = (settingId) => fetch('http://localhost:3000/api/settings', {
+export default const fetchSetting = (settingId) => fetch('http://192.168.2.88:3000/api/settings/', {
   method: 'GET',
   headers: {
     Accept: 'application/json',
@@ -10,3 +10,16 @@ export default const fetchSetting = (settingId) => fetch('http://localhost:3000/
     setting_id: 'settingId',
   }),
 });
+
+
+
+// export default const fetchSetting = (settingId) => fetch(`http://192.168.2.88:3000/api/settings/${settingId}`, {
+//   method: 'GET',
+//   headers: {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     setting_id: 'settingId',
+//   }),
+// });
