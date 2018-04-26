9fca50ad130aacbd586e15b48cdc5f0957e80974
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-04-25 17:12:06 -0700

    Remove body from fetchSetting

--- src/util/setting_api_util.js
+++ src/util/setting_api_util.js
@@ -4,13 +4,23 @@
 
 
 
-export default fetchSetting = (settingId) => fetch(`http://192.168.2.88:3000/api/settings/${settingId}`, {
+const fetchSetting = (settingId) => fetch(`http://192.168.2.88:3000/api/settings/${settingId}`, {
   method: 'GET',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    setting_id: 'settingId',
-  }),
+  }
 });
+
+// export default fetchSetting = (settingId) => fetch(`http://192.168.2.88:3000/api/settings/${settingId}`, {
+//   method: 'GET',
+//   headers: {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     setting_id: 'settingId',
+//   }),
+// });
+
+export default fetchSetting
