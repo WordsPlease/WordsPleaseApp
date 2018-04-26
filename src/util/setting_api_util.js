0110e61f5205dd28986d438282cf0dcc8c80ff8d
Andrew Tae <tiffanyw.hu@gmail.com>
2018-04-25 20:38:50 -0700

    Header

---
+++ src/util/setting_api_util.js
@@ -0,0 +1,25 @@
+// export default const fetchSetting = (settingId) => {
+//
+// }
+
+
+const fetchSetting = (settingId) => fetch(`http://words-please.herokuapp.com/api/settings/${settingId}`, {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  }
+});
+
+// export default fetchSetting = (settingId) => fetch(`http://192.168.2.88:3000/api/settings/${settingId}`, {
+//   method: 'GET',
+//   headers: {
+//     Accept: 'application/json',
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     setting_id: 'settingId',
+//   }),
+// });
+
+export default fetchSetting
