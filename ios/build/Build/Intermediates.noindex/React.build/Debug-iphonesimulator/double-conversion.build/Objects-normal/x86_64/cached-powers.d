349f0b6270d810943a1f4400311502f9a4999fb6
Jasper <jaspercheng.is@gmail.com>
2018-04-26 00:52:45 -0700

    Add thunk middleware

--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/double-conversion.build/Objects-normal/x86_64/cached-powers.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/double-conversion.build/Objects-normal/x86_64/cached-powers.d
@@ -1,5 +1,5 @@
 dependencies: \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/third-party/double-conversion-1.1.5/src/cached-powers.cc \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/third-party/double-conversion-1.1.5/src/utils.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/third-party/double-conversion-1.1.5/src/cached-powers.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/third-party/double-conversion-1.1.5/src/diy-fp.h
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/third-party/double-conversion-1.1.5/src/cached-powers.cc \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/third-party/double-conversion-1.1.5/src/utils.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/third-party/double-conversion-1.1.5/src/cached-powers.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/third-party/double-conversion-1.1.5/src/diy-fp.h
--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/third-party.build/Objects-normal/x86_64/vlog_is_on.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/third-party.build/Objects-normal/x86_64/vlog_is_on.d
@@ -1,12 +1,12 @@
 dependencies: \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/third-party/glog-0.3.4/src/vlog_is_on.cc \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/React/../third-party/glog-0.3.4/src/utilities.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/React/../third-party/glog-0.3.4/src/base/mutex.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/third-party/glog-0.3.4/src/config.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/React/../third-party/glog-0.3.4/src/config.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/React/../third-party/glog-0.3.4/src/glog/logging.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/React/../third-party/glog-0.3.4/src/glog/log_severity.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/React/../third-party/glog-0.3.4/src/glog/vlog_is_on.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/React/../third-party/glog-0.3.4/src/base/commandlineflags.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/React/../third-party/glog-0.3.4/src/glog/raw_logging.h \
-  /Users/appacademy/Desktop/WordsPleaseApp/node_modules/react-native/React/../third-party/glog-0.3.4/src/base/googleinit.h
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/third-party/glog-0.3.4/src/vlog_is_on.cc \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/React/../third-party/glog-0.3.4/src/utilities.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/React/../third-party/glog-0.3.4/src/base/mutex.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/third-party/glog-0.3.4/src/config.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/React/../third-party/glog-0.3.4/src/config.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/React/../third-party/glog-0.3.4/src/glog/logging.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/React/../third-party/glog-0.3.4/src/glog/log_severity.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/React/../third-party/glog-0.3.4/src/glog/vlog_is_on.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/React/../third-party/glog-0.3.4/src/base/commandlineflags.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/React/../third-party/glog-0.3.4/src/glog/raw_logging.h \
+  /Users/solipsistice/Desktop/words-please.nosync/node_modules/react-native/React/../third-party/glog-0.3.4/src/base/googleinit.h
